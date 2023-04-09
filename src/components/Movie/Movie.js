c8cdfce9b5d12db3f38688e283645df08e52a4d5
Malek-Abdelal <malekbassam1144@gmail.com>
2023-04-09 13:14:56 +0300

    ..

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -17,10 +17,6 @@ export default function Movie(props){
         <Card.Img style={{height :"400px"}} variant="top" src = {`https://image.tmdb.org/t/p/w500/${props.data.posterPath}`} />
         <Card.Body id="card-body">
             <Card.Title>{props.data.title}</Card.Title>
-            <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-            </Card.Text>
             <div id = 'button'>
             <Button id="add-fav-btn" variant="primary" onClick={handleShowModal}>Add to favorite list</Button>
             </div>
